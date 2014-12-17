39a7413589a0815cc9e55a0ad353959ffadf226c
jaime-h <jaime.e.hernandez@gmail.com>
2014-12-17 17:42:31 -0600

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Jaime Hernandez on 12/17/14.
+//  Copyright (c) 2014 Jaime Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by Jaime Hernandez on 12/17/14.
+//  Copyright (c) 2014 Jaime Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
