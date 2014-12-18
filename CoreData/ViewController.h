14b98b7d8ec8ce682adeb6a18c4f3bdc4511b2a6
jaime-h <jaime.e.hernandez@gmail.com>
2014-12-17 18:10:22 -0600

    Setting up th Gui & Connections

--- CoreData/ViewController.h
+++ CoreData/ViewController.h
@@ -9,7 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *name;
+@property (strong, nonatomic) IBOutlet UITextField *address;
+@property (strong, nonatomic) IBOutlet UITextField *phone;
+@property (strong, nonatomic) IBOutlet UILabel     *status;
 
+- (IBAction)saveData:(id)sender;
+- (IBAction)findContact:(id)sender;
 
 @end
 
